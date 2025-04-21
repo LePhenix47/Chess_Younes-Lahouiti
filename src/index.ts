3cb7068a45b5194ffbee3198960b9d760d217734
Younes Lahouiti <loller763@gmail.com>
2025-04-21 18:39:03 +0200

    Addition of rudimentary code to make the click event work

--- src/index.ts
+++ src/index.ts
@@ -14,17 +14,76 @@ import UserPointer from "@utils/classes/user-pointer.class";
 
 console.log(logarithm(1));
 
-const chessBoardElement: HTMLElement = selectQuery(".chess__board");
+const chessBoardElement: HTMLElement = selectQuery(
+  '[data-element="chess-board"]'
+);
 
 const chessBoardInstance: ChessBoard = new ChessBoard(chessBoardElement);
 
 const userPointer = new UserPointer(chessBoardElement);
 
-// TODO: Add an on click event
-// TODO: Must not forget to remove the "no-transition" class
+let lastPointerEvent: "drag" | "click" | null = null;
+
+// TODO: refactor code because it's a fucking mess, I should be able to just call a method
+chessBoardElement.addEventListener("click", (e: MouseEvent) => {
+  if (lastPointerEvent === "drag") {
+    lastPointerEvent = "click";
+    return;
+  }
+
+  const target = e.target as HTMLElement;
+
+  const targetIsChessPiece: boolean =
+    chessBoardInstance.elementIsChessPiece(target);
+
+  const targetIsSquare: boolean = !targetIsChessPiece;
+
+  const wasPreviouslySelectedPiece: boolean =
+    chessBoardInstance.elementIsPieceSelected(target);
+
+  if (chessBoardInstance.selectedPiece) {
+    const rankIndex: number = Number(target.dataset.rank);
+    const fileIndex: number = Number(target.dataset.file);
+    chessBoardInstance.updatePiecePosition(
+      chessBoardInstance.selectedPiece,
+      rankIndex,
+      fileIndex,
+      false
+    );
+
+    chessBoardInstance.clearSelectedPiece();
+
+    return;
+  }
+
+  if (targetIsSquare) {
+    return;
+  }
+
+  if (
+    !wasPreviouslySelectedPiece ||
+    !target.isSameNode(chessBoardInstance.selectedPiece)
+  ) {
+    chessBoardInstance.selectPiece(target);
+    return;
+  } else if (wasPreviouslySelectedPiece) {
+    chessBoardInstance.clearSelectedPiece();
+    return;
+  }
+
+  // * Clicked on a square thus we must move the piece to that square
+
+  // // TODO: Logic → If no piece was previously selected then select piece
+  // // TODO: If it was selected, on click the same piece, then unselect the piece
+  // TODO: If it was selected, on click a different piece, then unselect the previous piece and select the new one
+  // TODO: If it was selected, on click an empty square, then move the piece to that square
+
+  // ! Must update the logic as well in the DnD behavior to set the selected piece (TODO for the future after implementing the click logic)
+});
 
 // TODO: Refactor all of this to make a simple method call
 userPointer.on("custom:pointer-drag-move", (e) => {
+  lastPointerEvent = "drag";
   if (!userPointer.pressedElement.hasAttribute("data-position")) {
     return;
   }
@@ -37,7 +96,7 @@ userPointer.on("custom:pointer-drag-move", (e) => {
 
   chessBoardInstance.dragPiece(piece, pieceCursorOffsetX, pieceCursorOffsetY);
 
-  console.log({ pieceCursorOffsetX, pieceCursorOffsetY }, e);
+  console.log("custom:pointer-drag-move");
 });
 
 userPointer.on("custom:pointer-drag-end", (e) => {
@@ -52,12 +111,13 @@ userPointer.on("custom:pointer-drag-end", (e) => {
   const { lastRecordedPositions } = userPointer;
   const { containerX, containerY } = lastRecordedPositions;
 
-  let fileIndex: number = Math.floor(containerX / squareSize);
+  const fileIndex: number = Math.floor(containerX / squareSize);
 
-  let rankIndex: number = Math.floor(containerY / squareSize);
+  const rankIndex: number = Math.floor(containerY / squareSize);
 
   // TODO: Refactor code & create a method below
   chessBoardInstance.updatePiecePosition(piece, rankIndex, fileIndex, true);
+  chessBoardInstance.clearSelectedPiece();
 });
 
 console.log(userPointer);
