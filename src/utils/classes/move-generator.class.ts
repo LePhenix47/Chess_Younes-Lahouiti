744dbabb4b43f3a240c56c21141e0a3a6ccc3225
Younes Lahouiti <loller763@gmail.com>
2025-05-11 18:13:05 +0200

    Started addition of logic to pin pawns

--- src/utils/classes/move-generator.class.ts
+++ src/utils/classes/move-generator.class.ts
@@ -2,10 +2,10 @@ import AttacksGenerator from "./attacks-generator.class";
 import BaseMovesGenerator from "./base-moves-generator.class";
 import BoardController from "./board-controller";
 import BoardUtils from "./board-utils.class";
-import { AlgebraicNotation } from "./chess-board.class";
+import ChessBoard, { AlgebraicNotation } from "./chess-board.class";
 import Piece from "./piece.class";
 import Player from "./player.class";
-import RulesEngine from "./rules-engine.class";
+import RulesEngine, { PinnedPieceInfo } from "./rules-engine.class";
 
 export type SlidingPieceType = "rook" | "bishop" | "queen";
 
@@ -38,12 +38,35 @@ abstract class MovesGenerator {
   ): { moves: AlgebraicNotation[]; piece: Piece }[] {
     const result: { moves: AlgebraicNotation[]; piece: Piece }[] = [];
 
+    const king = ChessBoard.getPieceFromArray(
+      pieces,
+      "king",
+      player.color
+    ) as KingPiece;
+
+    const pinnedPieces: PinnedPieceInfo[] = RulesEngine.getPinnedPieces(
+      king,
+      pieces
+    );
+
     for (const piece of pieces.values()) {
       if (piece.color !== player.color) {
         continue;
       }
 
-      const moves = MovesGenerator.generateMoveForPiece(piece, pieces, player);
+      const potentialPinConstraint: PinnedPieceInfo | undefined =
+        pinnedPieces.find((pinnedInfo) => {
+          return Piece.arePiecesTheSame(pinnedInfo.pinned, piece);
+        });
+
+      console.log(potentialPinConstraint, piece);
+
+      const moves = MovesGenerator.generateMoveForPiece(
+        piece,
+        pieces,
+        player,
+        potentialPinConstraint
+      );
 
       if (moves.length > 0) {
         result.push({ piece, moves });
@@ -56,18 +79,39 @@ abstract class MovesGenerator {
   public static generateMoveForPiece = (
     piece: Piece,
     pieces: Map<AlgebraicNotation, Piece>,
-    player: Player
+    player: Player,
+    pinConstraint?: PinnedPieceInfo
   ): AlgebraicNotation[] => {
+    /**
+ // ! Temporary code: ↓
+*/
+
+    const pinnedPieces: PinnedPieceInfo[] = RulesEngine.getPinnedPieces(
+      ChessBoard.getPieceFromArray(pieces, "king", player.color) as KingPiece,
+      pieces
+    );
+
+    const potentialPinConstraint: PinnedPieceInfo | undefined =
+      pinnedPieces.find((pinnedInfo) => {
+        return Piece.arePiecesTheSame(pinnedInfo.pinned, piece);
+      });
+
+    /**
+ // ! Temporary code: ↑
+*/
+
     switch (piece.type) {
       case "pawn": {
         const pawnMoves = BaseMovesGenerator.generatePawnMoves(
           piece as PawnPiece,
-          pieces
+          pieces,
+          potentialPinConstraint
         ) as AlgebraicNotation[];
 
         const pawnAttacks = AttacksGenerator.getLegalPawnCaptures(
           piece as PawnPiece,
-          pieces
+          pieces,
+          potentialPinConstraint
         );
 
         return [...pawnMoves, ...pawnAttacks];
@@ -77,6 +121,7 @@ abstract class MovesGenerator {
         const knightMoves = BaseMovesGenerator.generateKnightMoves(
           piece as KnightPiece,
           pieces
+          // potentialPinConstraint
         ) as AlgebraicNotation[];
         return knightMoves;
       }
@@ -87,6 +132,7 @@ abstract class MovesGenerator {
         const slidingMoves = BaseMovesGenerator.generateSlidingMoves(
           piece as SlidingPiece,
           pieces
+          // potentialPinConstraint
         ) as AlgebraicNotation[];
         return slidingMoves;
       }
