477754758cd8cc73259fb52dd5218c78334d5365
Younes Lahouiti <loller763@gmail.com>
2025-06-15 11:53:22 +0200

    Fixed Vite's reference error

--- src/utils/classes/board-editor.class.ts
+++ src/utils/classes/board-editor.class.ts
@@ -3,9 +3,12 @@ import DragManager from "@utils/classes/drag-manager.class";
 import ChessBoard from "./chess-board.class";
 
 class BoardEditor extends ChessBoard {
+  // ? Note: Not extending ChessBoardController because of an issue with Vite
   constructor(container: HTMLElement) {
     super(container);
     this.generateBoard();
+
+    this.uiDragManager.setCallbacks({});
   }
 }
 
